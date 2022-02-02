d35e9015976c6880d83baf2fd9a2835ec9fabf93
Beka <55918745+bekatodua@users.noreply.github.com>
2022-02-02 11:05:54 +0400

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,7 +42,7 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
     
-    public function statistics();
+    public function statistics()
     {
         return $this->hasMany(CovidStatistic::class, 'foreign_user_ID', 'id');
     }
