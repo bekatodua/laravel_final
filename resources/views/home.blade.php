5f80c38a520647ce344fd9512c802cb3acd0c798
Beka <55918745+bekatodua@users.noreply.github.com>
2022-02-02 13:18:10 +0400

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,6 +15,34 @@
                     @endif
 
                     {{ __('You are logged in!') }}
+                    
+                    
+                    <div class="card-content">
+                            <div class="row">
+                                <div class="col-md-12 col-md-offset-1">
+                                    <div id="worldMap" style="height: 300px;"></div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <table class="table">
+                            <thead>
+                            <tr>
+                                <th scope="col">Country</th>
+                                <th scope="col">Date</th>
+                                <th scope="col">Case amount</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                @foreach($covid_stats as $row)
+                                <th>{{ $row->country }}</th>
+                                <td>{{ $row->date }}</td>
+                                <td>{{ $row->case_amount }}</td>
+                                @endforeach
+                            </tr>
+                            </tbody>
+                        </table>
 
 
 
